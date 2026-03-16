0ffa8074d4fabacc67ffaedc2f0e583f1bd5198f
maykon1227mu <maykon.lima1227@gmail.com>
2026-03-16 09:54:45 -0300

    aula02-variaveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,23 @@
+
+# Desafio 1
+nome = input("Digite seu nome ")
+
+print("Seja muito bem vindo", nome)
+
+#Atividade  2
+
+diaNas = input("Digite o dia do seu nascimento ")
+mesNas = input("Digite o mês do seu nascimento ")
+anoNas = input("Digite o ano do seu nascimento ")
+
+
+print("Você nasceu no dia ", diaNas, " do mês ", mesNas, " do ano ", anoNas)
+
+#Atividade 3
+
+num1 = int(input("Digite um número "))
+num2 = int(input("Digite outro número "))
+
+soma = num1 + num2
+
+print("A soma dos números é ", soma)
